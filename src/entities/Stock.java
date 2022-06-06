4e2d448bec1aa02f6274d36d16821a68aaefea29
Guilherme Frias <guilherme.frias.o.t3@gmail.com>
2022-06-06 18:22:05 -0300

    modifie README

--- src/entities/Stock.java
+++ src/entities/Stock.java
@@ -3,11 +3,14 @@ package entities;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import database.ConnectionFactory;
 
 public class Stock{
     public void insert() throws SQLException {
+        Connection conn = ConnectionFactory.getConnection();
+        Scanner input = new Scanner(System.in);
         
     }
 }
